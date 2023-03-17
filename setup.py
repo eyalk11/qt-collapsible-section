9dead87cb191eedbfeee5fb68648a5ec9c15b414
LAPTOP-G43MA5MO\ekarni <eyalk5@gmail.com>
2023-03-17 21:03:12 +0200

    Added Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='qt-collapsible-section',
+    version='',
+    packages=[''],
+    url='',
+    license='LGPL',
+    author='Michael A. Voelkel ',
+    author_email='michael.alexander.voelkel@gmail.com',
+    description='This is a simple collapsable section for PySide6',
+install_requires=
+    ['PySide6']
+)
