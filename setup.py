5d33e729f5cadf1852349b15fcdc06af6270c3e6
eyalk11 <72234965+eyalk11@users.noreply.github.com>
2023-08-31 01:56:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='qt-collapsible-section',
+    name='qt-collapsible-section-pyside6',
     version='0.1.0',
     packages=['qt_collapsible_section'],
-    url='',
+    url='https://github.com/eyalk11/qt-collapsible-section',
     license='LGPL',
     author='Michael A. Voelkel ',
     author_email='michael.alexander.voelkel@gmail.com',
