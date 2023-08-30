3f6d8edefdff0faa30809e24a2f37b52aa4035a8
eyalk11 <72234965+eyalk11@users.noreply.github.com>
2023-08-31 01:55:58 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qt-collapsible-section',
-    version='',
+    version='0.1.0',
     packages=['qt_collapsible_section'],
     url='',
     license='LGPL',
